be2be166edaf67a9293e6927318ca8b4448c20bf
priyanikki06 <priyanikki06@gmail.com>
2025-06-26 00:02:57 +0530

    initial commit

---
+++ credit_card_details.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ccdb;
+use ccdb;
+
+select * from credit_card;
+
+
+
+SET SQL_SAFE_UPDATES = 1;
+SHOW VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile = 1;
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+
+
+
